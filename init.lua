985726345cb803e1b37cbac1ebf9ddc79ce4adff
LRichard91 <140975381+LRichard91@users.noreply.github.com>
2023-11-28 13:25:27 +0100

    Use lualine icons and add nvim-web-devicons
        Modified lualine config to use icons and added necessary config to
        pull in nvim-web-devicons so lualine will display filetype devicons.

--- init.lua
+++ init.lua
@@ -162,12 +162,17 @@ require('lazy').setup({
   },
 
   {
+    'nvim-tree/nvim-web-devicons',
+    opt = false,
+  },
+
+  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
